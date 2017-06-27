b2612771604453f2d8d08e3aba9fcb90a83fde1d
Christian Gaege <christiangaege@gmail.com>
2017-06-27 09:06:11 +0200

    Provoke failed karma test

--- frontend-maven-plugin/src/it/example project/src/test/javascript/squareSpec.js
+++ frontend-maven-plugin/src/it/example project/src/test/javascript/squareSpec.js
@@ -1,5 +1,5 @@
 describe('The square function', function(){
     it('should square a number', function(){
-        expect(square(3)).toBe(9);
+        expect(square(3)).toBe(8); // provoke a build failure
     });
 });
